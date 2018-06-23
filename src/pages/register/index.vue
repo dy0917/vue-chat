60ebe6ea831a570a62a568d0c2f1f618e3bac1fb
Wenchong <huangkingsley@gmail.com>
2018-06-23 20:33:29 +1200

    init

---
+++ src/pages/register/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+      <div class="panel panel-info" >
+        <div class="panel-heading">
+          <div class="panel-title">Sign Up</div>
+        </div>
+
+        <div style="padding-top:30px" class="panel-body" >
+
+          <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+          <form class="form-horizontal" role="form">
+
+            <div style="margin-bottom: 25px" class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+              <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="email">
+            </div>
+
+            <div style="margin-bottom: 25px" class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+              <input id="password" type="password" class="form-control" name="password" placeholder="password">
+            </div>
+
+            <div style="margin-bottom: 25px" class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+              <input id="confirm-password" type="password" class="form-control" name="password" placeholder="password">
+            </div>
+
+            <div class="input-group">
+              <div class="checkbox">
+                <label>
+                  <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+                </label>
+              </div>
+            </div>
+
+            <div style="margin-top:10px" class="form-group">
+
+              <div class="col-sm-12 controls">
+                <a id="btn-login" href="#" class="btn btn-success">Login  </a>
+                <!--<a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>-->
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12 control">
+                <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                  Have an account!
+                  <router-link to="/"> Sign In</router-link>
+                </div>
+              </div>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
